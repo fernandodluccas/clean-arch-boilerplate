e44d9e06f0beab990d940e99b596976579905407
Fernando D'luccas <dluccash@hotmail.com>
2024-12-28 17:22:23 -0300

    feat(use-case): add create-user use case

---
+++ src/application/use-cases/create-user.ts
@@ -0,0 +1,12 @@
+import { IUserRepository } from "../../domain/repositories/i-user-repository";
+
+export class CreateUser {
+    constructor(private userRepository: IUserRepository) { }
+
+    async execute(name: string, email: string) {
+        const userExists = await this.userRepository.findByEmail(email);
+        if (userExists) throw new Error("User already exists");
+
+        return this.userRepository.create({ name, email });
+    }
+}
