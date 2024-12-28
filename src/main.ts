3255b93f46df25db5324af0df20b23a40bbcf7db
Fernando D'luccas <dluccash@hotmail.com>
2024-12-28 17:22:01 -0300

    feat: add main entry point for application

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import "reflect-metadata";
+import express from "express";
+import { routes } from "./presentation/routes";
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
